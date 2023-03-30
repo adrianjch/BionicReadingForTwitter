c8a623fb5470ef2cf24d716185dbfd12b35c44c1
Ikunobu <adrianjimenezchincolla@gmail.com>
2023-03-30 19:21:46 +0200

    Looping bold all text

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-document.title = "LOL!"
+setTimeout(test, 1000);
+
+
+function test()
+{
+	document.title = "LOL!"
+
+	const elements = document.getElementsByClassName('css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0');
+
+	for (let i = 0; i < elements.length; i++)
+	{
+		elements[i].innerHTML = '<b>' + elements[i].innerHTML + '</b>';
+		//elements[i].innerHTML = "stfu";
+	}
+	setTimeout(test, 1000);
+}
